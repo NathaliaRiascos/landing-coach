6a786ef8deff292b6b85065d08ee1ca05c86d643
NathaliaRiascos <riascosnathalia6@gmail.com>
2025-08-03 18:35:50 -0500

    fix: eslint and typescript

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,14 @@ export default [
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: eslintPluginAstro.parser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser", // para TypeScript en Astro
+        extraFileExtensions: [".astro"],
+      },
+    },
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
